4dab30a3d92b9ad3648726c606a56518ddce229e
NikDeKur <125550021+NikDeKur@users.noreply.github.com>
2024-04-08 21:31:29 +0100

    Double Jump system was fully added

--- src/main/kotlin/org/ndk/godsimulator/listener/DoubleJumpManager.kt
+++ src/main/kotlin/org/ndk/godsimulator/listener/DoubleJumpManager.kt
@@ -8,50 +8,126 @@ import org.bukkit.event.player.PlayerGameModeChangeEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.event.player.PlayerToggleFlightEvent
+import org.bukkit.util.Vector
 import org.ndk.godsimulator.GodSimulator.Companion.scheduler
-import org.ndk.minecraft.Utils.debug
+import org.ndk.godsimulator.event.profile.ProfileStaminaChangeEvent
+import org.ndk.godsimulator.profile.PlayerProfile.Companion.profile
 import org.ndk.minecraft.extension.cancel
 import org.ndk.minecraft.modules.PluginModule
+import org.ndk.minecraft.movement.OptiPlayerMoveEvent
+import java.util.*
 
 object DoubleJumpManager : Listener, PluginModule {
     override val id: String = "DoubleJumpManager"
 
+    fun updateBukkitState(player: Player) {
+        player.allowFlight = !(disallowJumpsJumped.contains(player.uniqueId) || disallowJumpsStamina.contains(player.uniqueId))
+    }
+
+    val disallowJumpsStamina = HashSet<UUID>()
+    fun allowJumpStamina(player: Player) {
+        val profile = player.profile
+        if (profile.stamina < 20) return
+        disallowJumpsStamina.remove(player.uniqueId)
+        updateBukkitState(player)
+    }
+
+    fun disallowJumpStamina(player: Player) {
+        disallowJumpsStamina.add(player.uniqueId)
+        updateBukkitState(player)
+    }
+
+    val disallowJumpsJumped = HashSet<UUID>()
+    fun allowJumpJumped(player: Player) {
+        if (!player.isOnGround) return
+        disallowJumpsJumped.remove(player.uniqueId)
+        updateBukkitState(player)
+    }
+
+    fun disallowJumpJumped(player: Player) {
+        disallowJumpsJumped.add(player.uniqueId)
+        updateBukkitState(player)
+    }
+
+
     /**
      * Make player double jump
      *
-     * Do not do any checks, just make player double jump
+     * Takes 20 players' stamina
      *
      * @param player The player to double jump
+     * @return True if player double jumped, false if player can't double jump
      */
-    fun doubleJump(player: Player) {
-        debug("fire double jump")
-        scheduler.runTask {
-            player.velocity = player.eyeLocation.direction.multiply(1.2)
-        }
+    fun doubleJump(player: Player): Boolean {
+        if (disallowJumpsJumped.contains(player.uniqueId)) return false
+        if (disallowJumpsStamina.contains(player.uniqueId)) return false
+        if (!player.profile.takeStamina(20)) return false
+
+        val direction = player.eyeLocation.direction
+        val x = direction.x
+        val z = direction.z
+        val y = 0.3
+        val vector = Vector(x, y, z)
+
+        player.velocity = player.velocity.add(vector)
+
+        disallowJumpJumped(player)
+        return true
     }
 
     @EventHandler
     fun onJoin(event: PlayerJoinEvent) {
-        event.player.allowFlight = true
+        allowJumpJumped(event.player)
+        allowJumpStamina(event.player)
     }
 
     @EventHandler
     fun onPlayerGamemodeChange(event: PlayerGameModeChangeEvent) {
         scheduler.runTask {
-            event.player.allowFlight = true
+            allowJumpJumped(event.player)
+            allowJumpStamina(event.player)
         }
     }
 
     @EventHandler
     fun onQuit(event: PlayerQuitEvent) {
-        event.player.allowFlight = false
+        disallowJumpJumped(event.player)
+        disallowJumpStamina(event.player)
     }
+
+
+
+    @EventHandler
+    fun onOptiMove(event: OptiPlayerMoveEvent) {
+        val player = event.player
+        allowJumpJumped(player)
+    }
+
+
+    @EventHandler
+    fun onStaminaChange(event: ProfileStaminaChangeEvent) {
+        val player = event.profile.onlinePlayer ?: return
+        if (event.newStamina < 20) {
+            disallowJumpStamina(player)
+        } else {
+            allowJumpStamina(player)
+        }
+    }
+
 
     @EventHandler
     fun onDoubleJump(event: PlayerToggleFlightEvent) {
-        if (event.player.gameMode == GameMode.CREATIVE) return
-        event.cancel()
         val player = event.player
-        doubleJump(player)
+        if (player.gameMode in bypassGamemodes) return
+        event.cancel()
+
+
+        // Bukkit may don't relly like, that we're using this event in survival,
+        // so wait for a final result and do our things
+        scheduler.runTask {
+            doubleJump(player)
+        }
     }
+
+    val bypassGamemodes = arrayOf(GameMode.CREATIVE, GameMode.SPECTATOR)
 }
