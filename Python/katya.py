8546e403a01358e5432a61db1257ec5fcd324aa5
NikDeKur <125550021+NikDeKur@users.noreply.github.com>
2024-04-08 19:19:51 +0100

    useless python scripts removed

--- Python/katya.py
+++
@@ -1,7 +0,0 @@
-возраст = int(input("How old are you? "))
-if возраст < 18:
-    print("You are not an adult")
-else:
-    print("You are an adult")
-
-
--- Python/wslTest.py
+++
@@ -1,5 +0,0 @@
-
-
-with open(r"\\wsl.localhost\Ubuntu\var\lib\pterodactyl\volumes\8ede2454-5c5a-4ca7-949f-58e99a16681e\server.properties", "r") as it:
-    print(it.read())
-
