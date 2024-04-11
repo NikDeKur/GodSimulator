e837b7538e0b7a68787620e8e64d50e654a28cd5
NikDeKur <125550021+NikDeKur@users.noreply.github.com>
2024-04-11 10:23:10 +0100

    To class Skill added god field and skillId field instead of abstract id. Now id is {god.id}_{skillId}; Added description field; Interface generates Icon now.

--- src/main/kotlin/org/ndk/godsimulator/skill/Skill.kt
+++ src/main/kotlin/org/ndk/godsimulator/skill/Skill.kt
@@ -1,18 +1,38 @@
 package org.ndk.godsimulator.skill
 
+import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 import org.ndk.global.interfaces.Snowflake
+import org.ndk.godsimulator.extension.setTexture
+import org.ndk.godsimulator.god.God
+import org.ndk.godsimulator.language.MSGDescriptionHolder
 import org.ndk.godsimulator.language.MSGNameHolder
+import org.ndk.minecraft.item.ItemPattern
 
-interface Skill : MSGNameHolder, Snowflake<String> {
+interface Skill : MSGNameHolder, MSGDescriptionHolder, Snowflake<String> {
+    val god: God
+    val skillId: String
     val executionClass: Class<out SkillExecution>
     val requiredRebirth: Int
     val requiredLevel: Int
     val cooldownMs: Long
 
+    override val id: String
+        get() = "${god.id}_$skillId"
+
     fun newInstance(executor: Player): SkillExecution {
         val constructor = executionClass.getConstructor(Player::class.java)
         return constructor.newInstance(executor)
+    }
+
+    override fun getIcon(player: Player): ItemStack {
+        return ItemPattern.from(Material.BARRIER)
+            .setDisplayName(nameMSG)
+            .setLore(descriptionMSG)
+            .setTag("god", id)
+            .setTexture(id)
+            .build(player, getFinalPlaceholder(player))
     }
 
     override val defaultPhName: String
